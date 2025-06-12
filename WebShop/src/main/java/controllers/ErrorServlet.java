02b83ebce59866c0cfed074288d407d21a357e5e
Zenslyy <22130275@st.hcmuaf.edu.vn>
2025-06-12 09:57:09 +0700

    Create ErrorServlet.java

---
+++ WebShop/src/main/java/controllers/ErrorServlet.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/CannotAccess")
+public class ErrorServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+	    resp.setCharacterEncoding("UTF-8");
+	    resp.setContentType("text/html; charset=UTF-8");
+		RequestDispatcher dispatcher=req.getRequestDispatcher("/Eror.jsp");
+		dispatcher.forward(req, resp);
+	}
+}
